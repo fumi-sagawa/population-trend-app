01a4b6d133be99aa41abc23f7f0f8ed8715d81da
sagawa <sagawa.fumi@gmail.com>
2021-07-04 18:39:39 +0900

    人口推移入手用のAPI Routeを作成

---
+++ pages/api/population/[id].ts
@@ -0,0 +1,22 @@
+import { NextApiHandler } from 'next';
+
+const handler: NextApiHandler = async (req, res) => {
+  console.log('population api called');
+  console.log(req.query.id);
+  const url = `${process.env.REASAS_URL_POPULATION}${req.query.id}`;
+  console.log(url);
+  const response = await fetch(url, {
+    headers: { 'X-API-KEY': process.env.X_API_KEY },
+  });
+  const data = await response.json();
+  console.log(data);
+  //REASAS_apiの人口構成から総人口推移を抜粋
+  const populationTrend = await data.result.data[0].data.map(
+    (prefectureData) => {
+      return prefectureData.value;
+    },
+  );
+  await res.status(200).json(populationTrend);
+};
+
+export default handler;
